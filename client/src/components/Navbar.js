2e16485fdd142f76e7e5b7d975e6ce7d92bedf5b
MNikhil2004 <mnikhil0904@gmail.com>
2024-06-20 23:59:37 +0530

    Navbar change 2

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -20,7 +20,7 @@ export default function Example() {
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
               <img
-                className="h-8 w-auto display-flex"
+                className="h-8 w-auto display-flex mx-80"
                 src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                 alt=""
               />
